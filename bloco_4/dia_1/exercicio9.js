590bfea3d3e9620e35a8653971700c2008949ec7
Diego F Affonso <diegofernandesaffonso@gmail.com>
2020-08-28 01:05:51 -0300

    Exercicio 9

---
+++ bloco_4/dia_1/exercicio9.js
@@ -0,0 +1,10 @@
+let numero1 = 12
+let numero2 = 32
+let numero3 = 10
+
+if(numero1 % 2 != 0 || numero2 % 2 != 0 || numero3 % 2 != 0){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
