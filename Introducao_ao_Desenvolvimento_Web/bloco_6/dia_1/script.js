38339e4346ad15b5a32d93929cc90126be2e5015
Diego F Affonso <diegofernandesaffonso@gmail.com>
2020-09-23 19:21:14 -0300

    Atividades do dia

---
+++ Introducao_ao_Desenvolvimento_Web/bloco_6/dia_1/script.js
@@ -0,0 +1,40 @@
+window.onload = gerarEstados;
+
+function gerarEstados() {
+    let selecionarEstados = document.querySelector("#estado");
+    let estadosDoBrasil = {
+        'AC': 'Acre',
+        'AL': 'Alagoas',
+        'AP': 'Amapá',
+        'AM': 'Amazonas',
+        'BA': 'Bahia',
+        'CE': 'Ceará',
+        'DF': 'Distrito Federal',
+        'ES': 'Espírito Santo',
+        'GO': 'Goiás',
+        'MA': 'Maranhão',
+        'MT': 'Mato Grosso',
+        'MS': 'Mato Grosso do Sul',
+        'MG': 'Minas Gerais',
+        'PA': 'Pará',
+        'PB': 'Paraíba',
+        'PR': 'Paraná',
+        'PE': 'Pernambuco',
+        'PI': 'Piauí',
+        'RJ': 'Rio de Janeiro',
+        'RN': 'Rio Grande do Norte',
+        'RS': 'Rio Grande do Sul',
+        'RO': 'Rondônia',
+        'RR': 'Roraima',
+        'SC': 'Santa Catarina',
+        'SP': 'São Paulo',
+        'SE': 'Sergipe',
+        'TO': 'Tocantins'
+    };
+    for (index in estadosDoBrasil) {
+        let opcoes = document.createElement("option");
+        opcoes.value = index;
+        opcoes.text = estadosDoBrasil[index];
+        selecionarEstados.appendChild(opcoes);
+    }    
+}
